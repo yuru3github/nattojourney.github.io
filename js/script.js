30497cfbceb8b74b3f06476447820a06c8c431f8
yuru3github <160844493+yuru3github@users.noreply.github.com>
2024-10-28 19:14:48 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,24 +43,55 @@ $('.match').ready(function() {
     }
 });
 
-$('.matching').ready(function() {
-    var hash = window.location.hash;
-    if (hash) {
-        $('.matching').animate({
-            scrollTop: $(hash).offset().top()
-        }, 1000);
-    }
-});
-
 $(".btn").on("click", function () {
     $(this).closest("div").css("display", "none");
     id = $(this).attr("href");
     $(id).addClass("fit").fadeIn("slow").show();
 });
 
-$('.match').ready(function() {
-    $('.anim').fadeIn(3000);
+var box =[];
+$(".btn").each(function(){
+    $(this).on('click',function(){
+        var value = $(this).data("value");
+        box.push(value);
+    });
 });
+
+$('.end').on('click',function(){
+    if( box == aceg ) {
+        $('#answer_01').css("display","");
+    } else if ( box == aceh ){
+        $('#answer_02').css("display","");
+    } else if ( box == acfg ){
+        $('#answer_03').css("display","");
+    } else if ( box == acfh ){
+        $('#answer_04').css("display","");
+    } else if ( box == adeg ){
+        $('#answer_05').css("display","");
+    } else if ( box == adeh ){
+        $('#answer_06').css("display","");
+    } else if ( box == adfg ){
+        $('#answer_07').css("display","");
+    } else if ( box == adfh ){
+        $('#answer_08').css("display","");
+    } else if ( box == bceg ){
+        $('#answer_09').css("display","");
+    } else if ( box == bceh ){
+        $('#answer_10').css("display","");
+    } else if ( box == bcfg ){
+        $('#answer_11').css("display","");
+    } else if ( box == bcfh ){
+        $('#answer_12').css("display","");
+    } else if ( box == bdeg ){
+        $('#answer_13').css("display","");
+    } else if ( box == bdeh ){
+        $('#answer_14').css("display","");
+    } else if ( box == bdfg ){
+        $('#answer_15').css("display","");
+    } else if ( box == bdfh ){
+        $('#answer_16').css("display","");
+    }
+  });
 
 $('.nippon area').ready(function() {
     var hash = window.location.hash;
