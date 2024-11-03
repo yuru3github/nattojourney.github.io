a7d857d8edc2a5121afe9db04e617fa6692f2b1b
yuru3github <160844493+yuru3github@users.noreply.github.com>
2024-11-03 12:21:48 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 $('.start').ready(function() {
         setTimeout(function() {
-            $('.start p').fadeIn(1600);
-        }, 500);
-        setTimeout(function() {
             $('.start').fadeOut(500);
         }, 2500);
 });
