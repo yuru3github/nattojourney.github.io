684f586c8197b7a585bf1713004b94f2616ed326
yuru3github <160844493+yuru3github@users.noreply.github.com>
2024-12-10 14:24:36 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,11 +47,20 @@ $('.btn').click(function(event) {
 $('.end').click(function() {
     let result = box.join('');
 
-    function resetAnimation(el) {
-        el.removeClass('spin-in');
-        void el[0].offsetWidth;
-        el.addClass('spin-in');
-    }
+  document.addEventListener("DOMContentLoaded", function () {
+  　const animElements = document.querySelectorAll('.anim');
+
+  　animElements.forEach(el => {
+    　el.style.opacity = "1";
+    　el.classList.add('spin-in');
+  　});
+　});
+
+　function resetAnimation(el) {
+  　el.removeClass('spin-in');
+  　void el[0].offsetWidth;
+  　el.addClass('spin-in');
+　});
 
     if (result === "aceg") {
         let el = $('#answer_01 .anim');
